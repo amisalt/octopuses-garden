497448d57feaad521be6557b5e8599eb301b0e24
Salt <amira9madiyar@gmail.com>
2024-11-07 22:04:42 +0500

    gamePage components restructure

---
+++ client/src/pages/Game/components/Device/Device.jsx
@@ -0,0 +1,44 @@
+import React, { useMemo, useRef, useState } from 'react'
+import "./Device.css"
+import { MyProgressCircular } from '../../../../components/informationals/ProgressBar/MyProgress'
+
+export function Device({food, grabItem, cooldown}) {
+  const [ready, setReady] = useState(false)
+  const [cookingProgress, setCookingProgress] = useState(0)
+  const deviceBox = useRef(null)
+  const position = useMemo(()=>{
+    if(deviceBox.current){
+      const rect = deviceBox.current.getBoundingClientRect()
+      return [rect.x, rect.y]
+    }
+    return [0,0]
+  }, [deviceBox])
+  function handleOnClick(){
+    console.log(cooldown, ready);
+    if(ready || cooldown === 0){
+      grabItem(food, position)
+      setReady(false)
+      setCookingProgress(0)
+    }else{
+      setCookingProgress(0.1)
+      let time = 0
+      const deciSeconds = setInterval(timer, 100)
+      function timer(){
+        time += 100
+        setCookingProgress(Math.round(time/cooldown*100))
+      }
+      setTimeout(()=>{
+        clearInterval(deciSeconds)
+        setReady(true)
+      }, cooldown)
+    }
+  }
+  return (
+    <div className='Device' ref={deviceBox} onClick={handleOnClick}>
+      <img src={`/api/image/devices/${food}.png`} alt={`${food} device`} className='Main'/>
+      { cookingProgress>0 && <div className='Status'>
+        { ready ? (<img src={`/api/image/food/${food}.png`} alt={`cooked ${food}`} className='Cooked'/>) : (<MyProgressCircular progress={cookingProgress}/>)}
+      </div>}
+    </div>
+  )
+}
