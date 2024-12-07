d8367f767874557dc6c04617acb5f68b084f04b2
Salt <amira9madiyar@gmail.com>
2024-12-07 19:55:42 +0500

    getting in and out of game

--- client/src/pages/Game/components/SettingsModal/SettingsModal.jsx
+++ client/src/pages/Game/components/SettingsModal/SettingsModal.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import "./SettingsModal.css"
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { MyButtonViolet } from '../../../../components/inputControls/MyButton/MyButton'
 import { useDispatch, useSelector } from 'react-redux'
 import { endQuery, exitQuery, setPause } from '../../../../app/store/slices/GamePlayedSlice'
@@ -11,10 +11,11 @@ import { removeGameDataHook } from '../../../../hooks/getDataHooks'
 
 export function SettingsModal({show, hideModal}) {
   const dispatch = useDispatch()
-  const {xp, money, xpOverall, moneyOverall, overallTime} = useSelector(state=>state.game)
+  const {xp, money, xpOverall, moneyOverall, overallTime, errors, message} = useSelector(state=>state.game)
   const {BGMvolume, SEvolume} = useSelector(state=>state.appData)
   const [BGMvolumeReactive, setBGMvolume] = useState(BGMvolume*100)
   const [SEvolumeReactive, setSEvolume] = useState(SEvolume*100)
+  const navigate = useNavigate()
 
   function handleChangeBGMvolume(value){
     setBGMvolume(value)
@@ -27,14 +28,19 @@ export function SettingsModal({show, hideModal}) {
 
   function handleEndQuery(){
     dispatch(endQuery({xp, money, overallTime, xpOverall, moneyOverall}))
-    dispatch(setGameState(false))
-    removeGameDataHook()
   }
   function handleExitQuery(){
     dispatch(exitQuery())
-    dispatch(setGameState(false))
-    removeGameDataHook()
   }
+
+  useEffect(()=>{
+    if(message === 'Success'){
+      if(['Progress saved','Game exited without saving progress'].includes(errors[0].msg)){
+        dispatch(setGameState(false))
+        navigate('/account')
+      }
+    }
+  }, [errors, message])
 
   useEffect(()=>{
     if(show) dispatch(setPause(true))
@@ -45,8 +51,8 @@ export function SettingsModal({show, hideModal}) {
       <MyButtonViolet width='100%' onClick={hideModal} id='SettingsModalButton'>Back to game</MyButtonViolet>
       <MySliderWithInput value={BGMvolumeReactive} setValue={handleChangeBGMvolume} width='90%' label='BGM volume' colorTheme='game'/>
       <MySliderWithInput value={SEvolumeReactive} setValue={handleChangeSEvolume} width='90%' label='SE volume' colorTheme='game'/>
-      <MyButtonViolet width='100%' onClick={handleExitQuery}><Link to='/account' className='Link'>Exit game</Link></MyButtonViolet>
-      <MyButtonViolet width='100%' onClick={handleEndQuery}><Link to='/account' className='Link'>Save & Exit</Link></MyButtonViolet>
+      <MyButtonViolet width='100%' onClick={handleExitQuery}>Exit game</MyButtonViolet>
+      <MyButtonViolet width='100%' onClick={handleEndQuery}>Save & Exit</MyButtonViolet>
     </div>
   )
 }
--- client/src/pages/Main/MainPage.jsx
+++ client/src/pages/Main/MainPage.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { MyButton } from '../../components/inputControls/MyButton/MyButton'
-import { startQuery } from '../../app/store/slices/GamePlayedSlice'
+import { createModeQuery, startQuery } from '../../app/store/slices/GamePlayedSlice'
 import "./MainPage.css"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { availableLevelsQuery } from '../../app/store/slices/GameInfoSlice'
 
 export function MainPage() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const availableLevels = useSelector(state=>state.gameInfo.levels.availableLevels)
+  const message = useSelector(state=>state.game.message)
   console.log(availableLevels)
 
   function handleStartQuery(levelId, mode){
     dispatch(startQuery({levelId, mode}))
+    if(message === 'Success'){
+      navigate(`/level/${levelId}`)
+    }
   }
 
   useEffect(()=>{
-    // dispatch(availableLevelsQuery())
-  })
+    dispatch(availableLevelsQuery())
+  }, [])
 
   return (
     <main className='page'>
@@ -25,7 +30,8 @@ export function MainPage() {
         <h1>Levels</h1>
         {availableLevels.map(level=><div className='Level'>
           <h2>{level.name}</h2>
-          <MyButton onClick={()=>handleStartQuery(level._id, level.mode)}><Link to={`/level/${level._id}`}>Start</Link></MyButton>
+          <p>{level.description}</p>
+          <MyButton onClick={()=>handleStartQuery(level._id, 'single')}><Link to={`/level/${level._id}`}>Start</Link></MyButton>
         </div>)}
       </div>
     </main>
