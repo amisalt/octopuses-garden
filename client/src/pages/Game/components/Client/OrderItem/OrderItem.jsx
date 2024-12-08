f6f8a2e901c4f804ed3b147056f79f904c36939e
Salt <amira9madiyar@gmail.com>
2024-12-08 17:14:07 +0500

    readable order ui

--- client/src/pages/Game/components/Client/OrderItem/OrderItem.jsx
+++ client/src/pages/Game/components/Client/OrderItem/OrderItem.jsx
@@ -4,7 +4,7 @@ import "./OrderItem.css"
 export function OrderItem({food, number}) {
   return (
     <div className='OrderItem' style={{display:number > 0 ? 'block' : 'none'}}>
-      <img src={`/api/image/food/${food}.png`} alt={food.png}/>
+      <div style={{backgroundImage:`url(/api/image/food/${food}.png)`}} className='Image'></div>
       <div className="Number"><p>{number}</p></div>
     </div>
   )
