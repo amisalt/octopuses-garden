5aaa7b2d581c396d59cfa1299ea97e441e69ed7f
Salt <amira9madiyar@gmail.com>
2024-12-11 21:37:25 +0500

    base for tutoial page link

--- client/src/pages/Main/MainPage.jsx
+++ client/src/pages/Main/MainPage.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { MyButton } from '../../components/inputControls/MyButton/MyButton'
+import { MyButton, MyButtonTransparent } from '../../components/inputControls/MyButton/MyButton'
 import { createModeQuery, startQuery } from '../../app/store/slices/GamePlayedSlice'
 import "./MainPage.css"
+import tutorial from "../../static/images/tutorialYellow.svg"
 import { Link, useNavigate } from 'react-router-dom'
 import { availableLevelsQuery } from '../../app/store/slices/GameInfoSlice'
 
@@ -25,13 +26,18 @@ export function MainPage() {
 
   return (
     <main className='page'>
-      <div className="Mainpage">
-        <h1>Levels</h1>
+      <div className="MainPage">
+        <section>
+          <h1>Levels</h1>
+          <MyButtonTransparent><img src={tutorial} alt="tuttorial button"/></MyButtonTransparent>
+        </section>
+        
         {availableLevels.map(level=><div className='Level'>
           <h2>{level.name}</h2>
           <p>{level.description}</p>
           <MyButton onClick={()=>handleStartQuery(level._id, 'single')}><Link to={`/level/${level._id}`}>Start</Link></MyButton>
         </div>)}
+        <Link to='/tutorial' className='TutorialLink'>Ready to Play? Learn the Essentials First!</Link>
       </div>
     </main>
   )
